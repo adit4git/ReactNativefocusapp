440a7d8b942598d817b9b422d078ce44caefb612
adit4git <adityamunjuluri@gmail.comm>
2021-05-30 13:17:27 -0400

    Initial commit

---
+++ src/features/Timer/Timing.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { RoundedButton } from '../../components/RoundedButton';
+
+export const Timing = ({ onChangeTime }) => {
+  return (
+    <>
+      <View style={styles.timingButton}>
+        <RoundedButton
+          title="10"
+          size={75}
+          onPress={() => onChangeTime(10)}></RoundedButton>
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton
+          title="15"
+          size={75}
+          onPress={() => onChangeTime(15)}></RoundedButton>
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton
+          title="20"
+          size={75}
+          onPress={() => onChangeTime(20)}></RoundedButton>
+      </View>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  timingButton: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
